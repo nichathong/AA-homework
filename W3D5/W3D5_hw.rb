7fff5803f302f8e4c5e12159380b92dcffa2354a
nichathong <nicha.fn@gmail.com>
2021-05-28 10:10:54 -0400

    Add @stack in initilaize

---
+++ W3D5/W3D5_hw.rb
@@ -0,0 +1,18 @@
+class Stack
+    def initialize
+      @stack = Array.new(n) { Array.new(n)}
+    end
+
+    def push(el)
+      # adds an element to the stack
+    end
+
+    def pop
+      # removes one element from the stack
+    end
+
+    def peek
+      # returns, but doesn't remove, the top element in the stack
+    end
+  end
+
