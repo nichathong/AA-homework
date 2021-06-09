7c0b4409bf82f242115bd458a2af7849fe8ea6f8
nichathong <nicha.fn@gmail.com>
2021-06-09 08:28:17 -0700

    Completed all

--- W5D3/skeleton/plays.rb
+++ W5D3/skeleton/plays.rb
@@ -33,10 +33,16 @@ class Play
   end
 
   def self.find_by_playwright(name)
+    data = PlayDBConnection.instance.execute(<<-SQL, name)
+    SELECT
+      *
+    FROM
+      Plays
+    WHERE
+      playwright_id = ?
+    SQL
   end
 
-  def self.find_by_name(name)
-  end
 
   def initialize(options)
     @id = options['id']
@@ -68,7 +74,70 @@ class Play
     SQL
   end
 
-  def get_plays
+  class Playwright 
+    attr_accessor :name, :id, :birth_year
+    def self.all
+      data = PlayDBConnection.instance.execute("SELECT * FROM playwrights")
+      data.map { |datum| Playwright.new(datum) }
+    end
+
+    def self.find_by_name(name)
+      raise "#{self} is already existed" unless @name
+      data = PlayDBConnection.instance.execute(<<-SQL, name)
+      SELECT
+        *
+      FROM
+        playwright
+      WHERE
+        name = ?
+      SQL
+      return nil if !person.length > 0
+
+    end
+
+    def new(options)
+      @id = options['id']
+      @name = options['name']
+      @birth_year = options['birth_year']
+
+    end
+
+    def create
+      raise "#{self} is already in database" unless self.id
+      PlayDBConnection.instance.execute(<<-SQL, self.name, self.birth_year)
+        INSERT INTO 
+        Playwright (id, name, birth_year)
+        VALUES
+        (?, ?)
+      SQL
+      self.id = PlayDBConnection.instance.last_insert_row_id
+
+    end
+
+    def update
+      raise "#{self} not in database" unless self.id
+      PlayDBConnection.instance.execute(<<-SQL, self.name, self.birth_year, self.id)
+        SELECT
+          name, birth_year
+        VALUES
+          name = ?, birth_year = ?
+        WHERE
+          id = ?
+      SQL
+
+    end
+
+    def get_plays
+      raise "#{self} is not in database" unless self.id
+      PlayDBConnection.instance.execute(<<-SQL, self.id)
+      SELECT 
+        *
+      FROM
+        Play
+      WHERE
+        playwright_id = ?
+      SQL
+    end
   end
 
 end
